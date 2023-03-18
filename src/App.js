d156de356aa7fb02102e41336ce66addc8829b11
Luis <luisrc91@live.com>
2023-03-18 13:56:44 -0400

    done with bonus

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+// Make sure to import the component we just built:
+import ColorBlock from './components/ColorBlock'
+import ColorForm from './components/ColorForm'
+
+function App(){
+    let [colors, setColors] = useState ( [
+        // 'violet', 'blue',
+        // 'lightblue', 'green',
+        // 'greenyellow', 'yellow',
+        // 'orange', 'red'
+    ])
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} key={i} />
+        )
+    })
+
+    const addColor = (newColor) => {
+      setColors([...colors, newColor])
+    }
+
+    return (
+        <div className="App">
+            {colors.map((color, i) => 
+        <ColorBlock key={i} color={color} />
+    )}
+
+            <ColorForm addColor ={addColor}/>
+        </div>
+    )
+}
+
+
+export default App;
+
+
+    
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+   
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/ColorForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+function ColorForm(props){
+    let [input, setInput] = useState('')
+
+    
+    const isColor = (strColor) => {
+        const s = new Option().style;
+        s.color = strColor;
+        return s.color !== '';
+    }
+
+    const handleSubmit = (e) => {
+        // We still want to eliminate the default behavior of our form submittal
+        e.preventDefault()
+        // addColor, the function we just built, should be available within props.
+        if (isColor(input)) {
+            props.addColor(input)
+        }
+        setInput('')
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" 
+                onChange={(e) => setInput(e.target.value)}
+                value={input} />
+                <button type="submit">Submit!</button>
+            </form>
+        </div>
+    )
+}
+
+export default ColorForm
